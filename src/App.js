2d6660307eb3985ef25bd7fc28425235b0345bb6
Tola's Terminal <sohbaker@gmail.com>
2019-02-08 13:56:38 +0000

    drafting second test for App class

--- src/App.js
+++ src/App.js
@@ -3,8 +3,27 @@ import React from "react";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: [],
+      errorMessage: ""
+    };
   }
+
+  async componentDidMount() {
+    try {
+      const response = await fetch("https://api.myjson.com/bins/9d960");
+      if (!response.ok) {
+        this.setState({ responseError: response.statusText });
+        throw Error(response.statusText);
+      }
+      const json = await response.json();
+      console.log(json);
+      this.setState({ data: json });
+    } catch (error) {
+      this.setState({ errorMessage: error });
+    }
+  }
+
   render() {
     return <div>Hello world</div>;
   }
