77154a69efd1249f31990a2a1b8e491e49e2e9ea
Elle Hallal <daniellehallal@gmail.com>
2019-02-11 13:01:22 +0000

    renders Item components based on data within this.state. Test passing

--- src/App.js
+++ src/App.js
@@ -5,37 +5,66 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
+      data: [
+        {
+          id: 0,
+          name: "Court Shoes, Nude Pink",
+          category: "Women's Footwear",
+          image: "https://i.imgur.com/fmUsxCO.jpg",
+          price: 9900,
+          shop_quantity: 5
+        },
+        {
+          id: 1,
+          name: "Suede Heels, Red",
+          category: "Women's Footwear",
+          image: "https://i.imgur.com/DVnYQl7.jpg",
+          price: 4200,
+          shop_quantity: 4
+        }
+      ],
       errorMessage: ""
     };
   }
 
-  async componentDidMount() {
-    try {
-      const response = await fetch("https://api.myjson.com/bins/9d960");
-      if (!response.ok) {
-        this.setState({ responseError: response.statusText });
-        throw Error(response.statusText);
-      }
-      const json = await response.json();
-      console.log(json);
-      this.setState({ data: json });
-    } catch (error) {
-      this.setState({ errorMessage: error });
+  handleClick = id => {
+    const { quantity } = this.state;
+    if (quantity > 0) {
+      quantity--;
+      this.setState({ quantity: quantity });
     }
-  }
+  };
+
+  // async componentDidMount() {
+  //   try {
+  //     const response = await fetch("https://api.myjson.com/bins/9d960");
+  //     if (!response.ok) {
+  //       this.setState({ responseError: response.statusText });
+  //       throw Error(response.statusText);
+  //     }
+  //     const json = await response.json();
+  //     console.log(json);
+  //     this.setState({ data: json });
+  //   } catch (error) {
+  //     this.setState({ errorMessage: error });
+  //   }
+  // }
 
   render() {
-    return (
-      <div>
+    const { data } = this.state;
+    const showProducts = data.map(data => {
+      return (
         <Item
-          name="hi"
-          price="200"
-          image="https://i.imgur.com/fmUsxCO.jpg"
-          category="Shoes"
-          quantity="2"
+          key={data.id}
+          id={data.id}
+          name={data.name}
+          price={data.price}
+          image={data.image}
+          category={data.category}
+          quantity={data.shop_quantity}
         />
-      </div>
-    );
+      );
+    });
+    return <div>{showProducts}</div>;
   }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -5,9 +5,30 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+let wrapper;
+let instance;
+
+beforeEach(() => {
+  wrapper = shallow(<App />);
+  instance = wrapper.instance();
+});
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+};
+
 test("renders without crashing", () => {
   shallow(<App />);
 });
+
+test("renders Item components based on data within this.state", () => {
+  const numberOfProducts = instance.state.data.length;
+  expect(wrapper.find("Item").length).toBe(numberOfProducts);
+});
+
+// test("deducts 1 from item quantity when add to cart button is clicked", () => {
+//   shallow(<App />);
+// });
 
 // test("makes an API call", async () => {
 //   const wrapper = shallow(<App />);
