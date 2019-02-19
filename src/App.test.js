e068d68ab70ecbb440277ff5ec904c81e58de59a
Tola's Terminal <sohbaker@gmail.com>
2019-02-19 16:17:46 +0000

    clicking the add to cart button decreases the shop quantity for item

--- src/App.test.js
+++ src/App.test.js
@@ -41,7 +41,7 @@ test("it fetches data from JSON file when server returns a successful response",
   );
 });
 
-test("deducts 1 from item quantity when add to cart button is clicked", async () => {
+test("clicking the add to cart button decreases the shop quantity for item", () => {
   const mockSuccessResponse = {
     products: [
       {
@@ -72,28 +72,15 @@ test("deducts 1 from item quantity when add to cart button is clicked", async ()
   const wrapper = mount(<App />);
   wrapper.setState({ data: mockSuccessResponse.products });
   const instance = wrapper.instance();
-  const itemShopQuantity = instance.state.data[0].shop_quantity;
-  const item = wrapper.find("Item").first();
-  instance.handleAddClick(instance.state.data[0].id);
-  expect(item.text()).toContain(itemShopQuantity - 1);
+
+  const shopQuantity = instance.state.data[0].shop_quantity;
+  const findFirstItem = wrapper.find("Item").first();
+  const button = findByTestAttr(findFirstItem, "Add to cart");
+  button.simulate("click");
+  wrapper.update();
+  const quantityDisplay = findByTestAttr(findFirstItem, "quantity");
+  expect(quantityDisplay.text()).toContain(shopQuantity - 1);
 });
-
-// test("when rendered it creates a cart_quantity key for existing items in shop data", () => {
-//   const cartQuantity = instance.state.data[1].cart_quantity;
-//   wrapper.debug();
-//   expect(cartQuantity).toEqual(0);
-// });
-
-// test("clicking the add to cart button decreases the shop quantity for item", () => {
-//   const wrapper = mount(<App />);
-//   const shopQuantity = instance.state.data[0].shop_quantity;
-//   const findFirstItem = wrapper.find("Item").first();
-//   const button = findByTestAttr(findFirstItem, "Add to cart");
-//   button.simulate("click");
-//   wrapper.update();
-//   const quantityDisplay = findByTestAttr(findFirstItem, "quantity");
-//   expect(quantityDisplay.text()).toContain(shopQuantity - 1);
-// });
 
 // test("resets shop_quantity when remove from cart button is clicked", () => {
 //   const redShoe = instance.state.data[1];
