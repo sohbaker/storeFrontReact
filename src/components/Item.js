9d5332b915362450c749dbb9334408b08fa6ef28
Elle Hallal <daniellehallal@gmail.com>
2019-02-20 11:47:23 +0000

    If item is out of stock, out-of-stock-text and out-of-stock-image css classes are applied to image and quantity text

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,11 +2,23 @@ import React from "react";
 import ItemButton from "./ItemButton";
 import "./item.css";
 
+function styleOutOfStockText(props) {
+  if (props.quantity === 0) {
+    return "out-of-stock-text";
+  }
+}
+
+function styleOutOfStockImage(props) {
+  if (props.quantity === 0) {
+    return "out-of-stock-image";
+  }
+}
+
 const Item = props => {
   return (
     <div className="ui card single-item">
       <div className="image">
-        <img test="image" className="item-image" src={props.image} alt={"image of " + props.name} />
+        <img test="image" className={"item-image " + styleOutOfStockImage(props)} src={props.image} alt={"image of " + props.name} />
       </div>
       <div className="content">
         <p test="name" className="header">
@@ -18,7 +30,7 @@ const Item = props => {
         <p test="price" className="description">
           £{props.price}
         </p>
-        <p test="quantity" className="extra-content">
+        <p test="quantity" className={"extra-content " + styleOutOfStockText(props)}>
           {props.quantity === 0 ? "Out of stock" : props.quantity + " remaining"}
         </p>
         <ItemButton onClick={props.onClick} quantity={props.quantity} />
