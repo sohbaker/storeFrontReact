e87d6195e8c4310efe66a6c9f42971369c1fefd8
Elle Hallal <daniellehallal@gmail.com>
2019-02-28 10:05:38 +0000

    Create express server

--- server/index.js
+++ server/index.js
@@ -2,3 +2,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser')
 
+const ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT || 5000;
+
+//initialise express and register middleware
+const app = express();
+
+//incoming requests with JSON payloads 
+app.use(express.json());
+//parses incoming requests with urlencoded payloads
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//make express responsive to requests
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`)
+})
+
+module.exports = app;
